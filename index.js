dc91156064164443480fd24bb99ed7da6ff8dab6
lucas <lucashfdantas@gmail.com>
2025-11-19 14:16:32 -0300

    instalando bootstrap e inicio do projeto

--- index.js
+++ index.js
@@ -7,28 +7,12 @@ app.use(express.static('public'));
 
 
 app.get("/",(req, res) => {
-    var nome ="lucas Henrique";
-    var lang = "javaScript";
-    var msg = false;
-
-
-    var produtos = [
-        {nome: "melancia", preco:"5"},
-        {nome: "uva", preco:"2.50"},
-        {nome: "maça", preco:"3"}
-    ]
-
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        empresa: "Jacto SA",
-        msg: msg,
-        produtos: produtos
-    });
+    
+res.render("index");
 });
 
 
 
-app.listen(3000,() =>{
+app.listen(4000,() =>{
     console.log("app rodando!")
 });
