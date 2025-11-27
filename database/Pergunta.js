6aefb3489a54dd5b6f04260a52c1b3287cff1f58
lucas <lucashfdantas@gmail.com>
2025-11-27 14:23:08 -0300

    gerando tabela com model

---
+++ database/Pergunta.js
@@ -0,0 +1,17 @@
+const sequelize = require('sequelize');
+const connection = require('./database');
+
+
+const pergunta = connection.define('pergunta',{
+    titulo:{
+        type: sequelize.STRING,
+        AllowNull: false
+    },
+    descricao:{
+        type: sequelize.TEXT,
+        AllowNull: false
+    }
+})
+
+
+pergunta.sync({force: false}).then(() =>{})
